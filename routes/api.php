020e571bdba337b7769b9db4bb7448e2c0bb5ddb
noeliamorato <cbbe.noelia.morato.ca@unifranz.edu.bo>
2023-06-14 09:48:18 -0400

    deploy

--- routes/api.php
+++ routes/api.php
@@ -8,25 +8,29 @@ use App\Http\Controllers\MascotasController;
 use App\Http\Controllers\PresentacionesController;
 use App\Http\Controllers\Propietario_mascotasController;
 use App\Http\Controllers\RolesController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\VeterinariosController;
 use App\Http\Controllers\VisitasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::get('registeruser', [UserController::class, 'Userregister']);
+
+Route::group(['middleware' => ["auth:sanctum"]], function () {
+    Route::get('users-profile', [UserController::class, 'userProfile']);
+    Route::get('logout', [UserController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::get('/Roles', [RolesController::class, 'index']);
 Route::post('/Roles', [RolesController::class, 'store']);
